7b3e82eccc7afa41d87b8156386e07c73998127a
ntoro <ntoro@ebi.ac.uk>
2019-05-29 16:08:34 +0100

    Externalise properties and prepare the application to run as .war file in tomcats.

--- src/main/java/uk/ac/ebi/intact/search/interactor/InteractorSearchServiceApplication.java
+++ src/main/java/uk/ac/ebi/intact/search/interactor/InteractorSearchServiceApplication.java
@@ -2,18 +2,30 @@ package uk.ac.ebi.intact.search.interactor;
 
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.solr.core.SolrOperations;
 import org.springframework.data.solr.core.SolrTemplate;
 
 @SpringBootApplication
-public class InteractorSearchServiceApplication {
+public class InteractorSearchServiceApplication extends SpringBootServletInitializer {
+
+	@Value("${spring.data.solr.host}")
+	private String solrHost;
+
+	//This enables the option to pack the app as a .war for external tomcats
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(InteractorSearchServiceApplication.class);
+	}
 
 	@Bean
 	public SolrClient solrClient() {
-		return new HttpSolrClient("http://localhost:8983/solr");
+		return new HttpSolrClient.Builder(solrHost).build();
 	}
 
 	@Bean
